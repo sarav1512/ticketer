396e37dedf85892eba4ba3419434b938d6d26d87
srathinakumar <srathinakumar@visolve.com>
2015-06-04 03:08:21 +0530

    ticketer application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '7f4b87a537805ca7cfbed855bd870fd27ca1a10b100824237ee27fca1082e1a0975f88ac30d20dfd543e9f08bde87c4792fa996523b840377801030c387de4e0'
